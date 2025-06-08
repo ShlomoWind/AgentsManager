ad6c2f6a2bce1f2c8a7d4b95650d693333e78c92
Shlomo <sh.z.wind@gmail.com>
2025-06-08 11:31:02 +0300

    Add project files.

---
+++ AgentsManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentsManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
