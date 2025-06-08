7ea18cf4df0793149235c8b1ce9cd4f151356b61
Shlomo <sh.z.wind@gmail.com>
2025-06-08 15:45:02 +0300

    create the AgentDAL with the metodes

---
+++ AgentsManager/DAL/AgentDAL.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+internal class AgentDAL
+{
+    private string connStr = "server=localhost;user=root;password=;database=eagleeyedb";
+    private MySqlConnection conn;    
+    public AgentDAL()
+    {
+        this.conn = new MySqlConnection(connStr); 
+    }
+    public MySqlCommand Command(string query)
+    {
+        MySqlCommand cmd = new MySqlCommand(query, this.conn);
+        return cmd;
+
+    }
+    private void AddAgent(Agent agent)
+    {
+        string query = $"INSERT INTO eagleeyedb (id, codeName, realNam, location, status,missionsCompleted) VALUES ({agent.id},{agent.codeName},{agent.realNam},{agent.location},{agent.status},{agent.missionsCompleted})";
+        this.Command(query);
+    }
+    private List<Agent> GetAllAgents()
+    {
+        string query = $"SELECT * FROM eagleeyedb";
+        List<Agent> AllAgents = new List<Agent>();
+        var cmd =  this.Command(query);
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Agent agent = new Agent(
+                reader.GetInt32("id"),
+                reader.GetString("codeName"),
+                reader.GetString("realName"),
+                reader.GetString("location"),
+                reader.GetString("status"),
+                reader.GetInt32("missionsCompleted")
+                );
+            AllAgents.Add(agent);
+        }
+        return AllAgents;
+    }
+    private void UpdateAgentLocation(int AgentId, string newLocation)
+    {
+        string query = $"UPDATE eagleeyedb location={newLocation} WHERE id={AgentId}";
+        this.Command(query);
+    }
+    private void DeleteAgent(int agentId)
+    {
+        string query = $"DELETE FROM eagleeyedb WHERE id={agentId}";
+        this.Command(query);
+    }
+} 
--- AgentsManager/Models/Agent.cs
+++ AgentsManager/Models/Agent.cs
@@ -1,12 +1,20 @@
-﻿class Agent
+﻿internal class Agent
 {
-    private int id { get; set;}
-    private string codeName { get; set;}
-    private string realNam { get; set;}
-    private string location { get; set;}
-    private string status { get; set;}
-    private int missionsCompleted { get; set;}
+    public int id { get; set;}
+    public string codeName { get; set;}
+    public string realNam { get; set;}
+    public string location { get; set;}
+    public string status { get; set;}
+    public int missionsCompleted { get; set;}
 
-    public Agent(int id, string codeName, string realName, string location, string status, int missionCompleted) { }
+    public Agent(int id, string codeName, string realName, string location, string status, int missionCompleted) 
+    {
+        this.id = id;
+        this.codeName = codeName;
+        this.realNam = realName;
+        this.location = location;
+        this.status = status;
+        this.missionsCompleted = missionCompleted;
+    }
 
 }
