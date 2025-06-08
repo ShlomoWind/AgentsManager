18bff10fc19bbfc2a6af294588571f9378312407
Shlomo <sh.z.wind@gmail.com>
2025-06-08 12:02:33 +0300

    create class Agent with the fields

---
+++ AgentsManager/Models/Agent.cs
@@ -0,0 +1,10 @@
+﻿class Agent
+{
+    private int id { get; set;}
+    private string codeName { get; set;}
+    private string realNam { get; set;}
+    private string location { get; set;}
+    private string status { get; set;}
+    private int missionsCompleted { get; set;}
+
+}
